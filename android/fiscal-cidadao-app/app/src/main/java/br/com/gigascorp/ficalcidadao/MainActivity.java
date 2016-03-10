8db255c4fa3041f18b2d53cd23f80634f53ddf96
Thiago Nunes de Sousa <thiagonunes.tns@gmail.com>
2016-03-10 20:51:59 -0300

    Adicionado mapa à aplicação centralizado e com marcador no ponto (0,0)

--- android/fiscal-cidadao-app/app/src/main/java/br/com/gigascorp/ficalcidadao/MainActivity.java
+++ android/fiscal-cidadao-app/app/src/main/java/br/com/gigascorp/ficalcidadao/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.gigascorp.ficalcidadao;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this, MapaConveniosActivity.class);
+        startActivity(intent);
     }
 
     @Override

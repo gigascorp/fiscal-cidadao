95cbbb56cd12819363b714749cab05f602a723e0
Thiago Nunes de Sousa <thiagonunes.tns@gmail.com>
2016-03-10 21:43:37 -0300

    Adicionando várias marcas ao mapa e setando o bound para exibir todas no mapa

--- android/fiscal-cidadao-app/app/src/main/java/br/com/gigascorp/ficalcidadao/MapaConveniosActivity.java
+++ android/fiscal-cidadao-app/app/src/main/java/br/com/gigascorp/ficalcidadao/MapaConveniosActivity.java
@@ -1,70 +1,51 @@
 package br.com.gigascorp.ficalcidadao;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapaConveniosActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+public class MapaConveniosActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private List<LatLng> coordenadas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //fixme
+        coordenadas = new ArrayList<>();
+        coordenadas.add(new LatLng(-2.532783, -44.291549));
+        coordenadas.add(new LatLng(-2.533962,-44.293427));
+        coordenadas.add(new LatLng(-2.530779,-44.294467));
+        coordenadas.add(new LatLng(-2.535677, -44.289585));
+
         setContentView(R.layout.activity_mapa_convenios);
-        setUpMapIfNeeded();
+
+        SupportMapFragment mapFragment = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map));
+        mapFragment.getMapAsync(this);
+
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        setUpMapIfNeeded();
-    }
-
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p>
-     * If it isn't installed {@link SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
-     */
-    private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
+    public void onMapReady(GoogleMap map) {
+        LatLngBounds.Builder builder = LatLngBounds.builder();
+        for(LatLng coord : coordenadas){
+            builder.include(coord);
+            map.addMarker(new MarkerOptions().position(coord).title(coord.toString()));
         }
-    }
 
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
-     */
-    private void setUpMap() {
-        LatLng procuradoria = new LatLng(-2.532783,-44.291549);
+        LatLngBounds bounds = builder.build();
 
-        mMap.addMarker(new MarkerOptions().position(procuradoria).title("Procuradoria Geral de Justiça"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(procuradoria));
+        map.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 40));
     }
 }

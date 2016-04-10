ef2120166551bbacbdd9f90e035bf0f66c20e188
Emilio <emilioweba@gmail.com>
2016-04-09 22:34:52 -0500

    Mudancas para marcio

---
+++ web/FiscalCidadaoWeb/Models/RetornoRanking.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FiscalCidadaoWCF.Models
+{
+    public class RetornoRanking
+    {
+        public int Count { get; set; }
+
+        public List<UsuarionRankingViewModel> Lista { get; set; }
+
+        public string Message { get; set; }
+
+        public int HttpStatus { get; set; }
+    }
+
+    public class UsuarionRankingViewModel
+    {
+        public string Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public double Pontuacao { get; set; }
+
+        public string UrlFoto { get; set; }
+    }
+
+}

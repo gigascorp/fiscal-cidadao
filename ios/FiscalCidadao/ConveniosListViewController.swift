0e7342fb53a7289e0a9bcf20ffa77e9fc6e3439a
guilhermeribeiro <guilherme.ufma@gmail.com>
2016-04-13 13:25:15 -0300

    Inclusão da Licença GPL v3 no código iOS.

--- ios/FiscalCidadao/ConveniosListViewController.swift
+++ ios/FiscalCidadao/ConveniosListViewController.swift
@@ -1,10 +1,21 @@
-//
-//  ConveniosListViewController.swift
-//  FiscalCidadao
-//
-//  Created by Wallas Henrique Santos on 19/03/16.
-//  Copyright © 2016 Wallas Henrique Santos. All rights reserved.
-//
+/*
+ Copyright (C) 2016  Andrea Mendonça, Emílio Weba, Guilherme Ribeiro, Márcio Oliveira, Thiago Nunes, Wallas Henrique
+ 
+ This file is part of Fiscal Cidadão.
+ 
+ Fiscal Cidadão is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Fiscal Cidadão is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with Fiscal Cidadão.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import UIKit
 
